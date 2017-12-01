4aa991aeb6758eccf1e617315894c9fc52b0a051
Daniel Tigse <danieltp15@hotmail.com>
2017-12-01 15:53:52 -0500

    Ed25519 exposed

--- SignalProtocolFramework/SignalProtocolFramework.h
+++ SignalProtocolFramework/SignalProtocolFramework.h
@@ -13,6 +13,8 @@
 #import "RatchetingSession.h"
 #import "SessionBuilder.h"
 #import "SessionCipher.h"
+#import "AxolotlExceptions.h"
+#import "Ed25519.h"
 
 //! Project version number for SignalProtocolFramework.
 FOUNDATION_EXPORT double SignalProtocolFrameworkVersionNumber;
